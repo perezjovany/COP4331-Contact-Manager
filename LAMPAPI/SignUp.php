f79f57a37b141e43919e5ce518ea712eea8a180e
Jovany Perez <perezjovany@gmail.com>
2023-05-28 20:38:13 -0400

    user already exist case

--- LAMPAPI/SignUp.php
+++ LAMPAPI/SignUp.php
@@ -1,5 +1,4 @@
 <?php
-
     $inData = getRequestInfo();
 	
     $firstName = $inData["firstName"];
@@ -8,25 +7,35 @@
     $password = $inData["password"];
 
     $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 	
-	if( $conn->connect_error )
-	{
-		returnWithError( $conn->connect_error );
-	}
-	else
-	{
-		$stmt = $conn->prepare("INSERT INTO Users (firstName, lastName, Login, Password) VALUES (?, ?, ?, ?)");
-		$stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
-		
-        if ($stmt->execute()) {
-            $id = $stmt->insert_id;
-            returnWithInfo($firstName, $lastName, $id);
+    if ($conn->connect_error) {
+        returnWithError($conn->connect_error);
+    } else {
+        // Check if user already exists
+        $stmt = $conn->prepare("SELECT * FROM Users WHERE Login = ?");
+        $stmt->bind_param("s", $login);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows > 0) {
+            // User already exists, return an error
+            returnWithError("User already exists", 409);
         } else {
-            returnWithError("Registration failed");
+            $stmt->close();
+
+            $stmt = $conn->prepare("INSERT INTO Users (firstName, lastName, Login, Password) VALUES (?, ?, ?, ?)");
+            $stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
+
+            if ($stmt->execute()) {
+                $id = $stmt->insert_id;
+                returnWithInfo($firstName, $lastName, $id);
+            } else {
+                returnWithError("Registration failed");
+            }
         }
-    
+
         $stmt->close();
         $conn->close();
-	}
+    }
 	
     function getRequestInfo()
     {
@@ -39,16 +48,16 @@
         echo $obj;
     }
     
-    function returnWithError($err)
+    function returnWithError($err, $responseCode = 400)
     {
+        http_response_code($responseCode);
         $retValue = '{"error":"' . $err . '"}';
         sendResultInfoAsJson($retValue);
     }
     
-    function returnWithInfo( $firstName, $lastName, $id )
-	{
-		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
-		sendResultInfoAsJson( $retValue );
-	}
-    
+    function returnWithInfo($firstName, $lastName, $id)
+    {
+        $retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+        sendResultInfoAsJson($retValue);
+    }
 ?>
